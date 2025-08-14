59f143a5718653c1d3bca83014f0ec19d517e09f
yddod_ <nohff1234@gmail.com>
2025-08-14 17:46:41 +0700

    Update

---
+++ app/components/page/post_job/hero1_section.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <section class="flex justify-between p-12">
+            <!-- Details -->
+            <div class="flex justify-evenly flex-col w-5/12 text-zinc-900">
+                <h1 class="text-6xl font-bold">Post a job today, hire tomorrow</h1>
+                <p class="text-xl">
+                    Connect with talent that gets you, and hire them to take your business to the next level.
+                </p>
+                <button class="btn-brand-blue w-9/12 p-3 text-xl">Get Started</button>
+            </div>
+
+            <!-- Image -->
+            <div>
+                <img src="https://static.vecteezy.com/system/resources/previews/004/652/447/non_2x/close-up-business-woman-using-calculator-and-laptop-for-do-math-finance-on-wooden-desk-in-office-and-business-working-background-tax-accounting-statistics-and-analytic-research-concept-free-photo.jpg"
+                    alt="image" width="650" height="650" />
+            </div>
+        </section>
+    </div>
+</template>
